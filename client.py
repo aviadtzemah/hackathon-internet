b986e65328cb58769cb7f0bf70a5b17fe039697a
talkad <talkad@post.bgu.ac.il>
2020-12-28 11:54:29 +0200

    craeted GUI for the client via pygame. the pygame package providing key-press handler, so now the keyboard is non-blocking

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket
-import time
-from getch import getch
 import struct
+from game import Game
+
 
 class Client:
     """
@@ -21,75 +21,75 @@ class Client:
         the client figures out the server address by the sourceIP and return the received message and sourceIP.
         """
         UDP_PORT = 13117
-        print('Client started, listening for offer requests...')
-        udp_socket = socket(socket.AF_INET, socket.SOCK_DGRAM)     # creating UDP socket
-        udp_socket.bind(('', UDP_PORT))                            # bind the socket to a certain port
-        msg, (host, _port) = udp_socket.recvfrom(2048)
+        udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # creating UDP socket
+        udp_socket.bind(('', UDP_PORT))  # bind the socket to a specific port
+        broadcast_msg, (hostIP, _port) = udp_socket.recvfrom(2048)
 
-        udp_socket.close()                                         # close the connection of the UDP socket
-        return host, msg
+        udp_socket.close()  # close the connection of the UDP socket
+        return hostIP, broadcast_msg
 
-    def extract_port(self, msg):
+    def extract_port(self, broadcast_msg):
         """
         The function checks if the received message is legal- the first 4 bytes are 0xfeedbeed
         and the next byte is 0x2.
         if the message legal, return the last 2 bytes (the tcp port). otherwise return -1.
         """
-        unpacked_msg = struct.unpack('lch', msg)  # unpacking message in format (long, char, short)
-        
+
+        try:
+            unpacked_msg = struct.unpack('lch', broadcast_msg)  # unpacking message in format (long, char, short)
+        except struct.error:
+            print('failed to unpack message -> illegal message')
+            return -1
+
         if unpacked_msg[0] != 0xfeedbeef:
             print(f'wrong magic cookie - expected to FEED BEEF and not {unpacked_msg[0]}')
             return -1
-            
+
         if unpacked_msg[1] != 0x2:
             print(f'wrong message type - 0x2 != {unpacked_msg[1]}')
             return -1
 
         return unpacked_msg[2]
 
-    def connect(self, host, tcp_port):
+    def connect(self, hostIP, tcp_port):
         """
         This function is trying to connect to server.
         If it succeeds return True. otherwise, False.
         """
-        print(f'Received offer from {host}, attempting to connect...')
+        print(f'Received offer from {hostIP}, attempting to connect...')
 
-        tcp_socket = socket(socket.AF_INET, socket.SOCK_STREAM)     # creating TCP socket
+        tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # creating TCP socket
         try:
-            tcp_socket.connect((host, tcp_port))
+            tcp_socket.connect((hostIP, tcp_port))
         except socket.error:
-            print(f'Failed to connect to server at address {host} and port {tcp_port}')
+            print(f'Failed to connect to server at address {hostIP} and port {tcp_port}')
             return False
 
-        tcp_socket.send(f'{self.team_name}\n')  # send the team name
+        tcp_socket.send(f'{self.team_name}\n'.encode('utf-8'))  # send the team name
         return True
 
     def play(self):
         """
         The game is starting- the client can insert keys for the duration of 10 seconds.
         """
-        msg = self.tcp_socket.recv(2048)
-        print(msg)       # print the welcome message
+        welcome_msg = self.tcp_socket.recv(2048)
+        print(welcome_msg)  # print the welcome message
 
-        self.tcp_socket.setblocking(False)   # set the socket to non-blocking
+        self.tcp_socket.setblocking(False)  # set the socket to non-blocking
+        game = Game(self)
 
-        starting_time = time.time()  # starting a timer
-        while time.time() - starting_time <= 10:
-            char = getch.getche()       # read single character from the user
-            self.tcp_socket.send(char)  # send the character to server
+        game.run()
 
-            msg = self.tcp_socket.recv(128)  # how it is going to single-threaded?
-            print(msg)
-
-        # not really waiting for the server to disconnect
         self.tcp_socket.close()
         print('Server disconnected, listening for offer requests...')
 
 
 if __name__ == "__main__":
 
+    print('Client started, listening for offer requests...')
+
     while True:
-        client = Client(team_name='Ribon_Haolamim')
+        client = Client(team_name='Ribon_Ha_Olamim')
         host, msg = client.find_server()
         port = client.extract_port(msg)
 
---
+++ game.py
@@ -0,0 +1,102 @@
+from service import *
+
+
+class Game:
+    def __init__(self, client):
+        # pygame configuration
+        pygame.init()
+        pygame.display.set_caption("Keyboard Spamming")
+        clock = pygame.time.Clock()
+        FPS = 30
+        clock.tick(FPS)
+
+        # game fields
+        self.client = client
+        self.text = ''
+        self.text_blocks = []
+        self.opponent_text = ''
+        self.opponent_text_blocks = []
+        self.game_over = False
+        self.current_text_height = 75
+        self.current_opponent_text_height = 245
+        self.start_time = pygame.time.get_ticks()
+        self.seconds = 10  # measure countdown
+
+    def event_handler(self):
+        self.key_pressed_handler()  # key pressed event handler
+        self.timer_handler()  # timer event handler
+        self.receive_msg()  # receive message handler
+
+    def key_pressed_handler(self):
+        """
+        When a key is pressed, the key will be sent to the server.
+        """
+        for event in pygame.event.get():
+
+            if event.type == pygame.KEYDOWN:  # the client hit the keyboard
+                self.text = concat_char(self.text, pygame.key.name(event.key))
+                self.client.tcp_socket.send(f'{pygame.key.name(event.key)}\n')  # send the character to server
+
+                if len(self.text) == 250:
+                    self.current_text_height += 20
+                    self.text_blocks.append(self.text)
+                    self.text = ''
+
+            # check for closing window
+            if event.type == pygame.QUIT:
+                self.game_over = True
+
+    def timer_handler(self):
+        """
+        This function calculte the current timer value and set game_over to true 
+        when the countdown finished. 
+        """
+        self.seconds = 10 - int((pygame.time.get_ticks() - self.start_time) / 1000)
+        if self.seconds == 0:
+            self.game_over = True
+
+    def receive_msg(self):
+        """
+        This function receives a message from the server in non-blocking manner.
+        if a message received it will be concatenate to the previous messages.
+        """
+        msg = self.client.tcp_socket.recv(128)
+        self.opponent_text = concat_char(self.text, msg)
+
+        if len(self.opponent_text) == 250:
+            self.current_opponent_text_height += 20
+            self.opponent_text_blocks.append(self.text)
+            self.opponent_text = ''
+
+    # draw all objects on the screen
+    def draw(self):
+        if not self.game_over:
+            # draw the background
+            screen.blit(background, (0, 0))
+
+            # draw headers
+            draw_text(screen, f'{self.seconds}', 50, WIDTH - 50, 10, 'red')
+            draw_text(screen, f'Battle Royal', 50, WIDTH / 2, 10, 'white')
+
+            # draw rects
+            draw_rect(10, 70, 150, 980)
+            draw_rect(10, 240, 250, 980)
+
+            # draw text
+            draw_text(screen, self.text, 14, WIDTH / 2, self.current_text_height, 'green')
+            draw_msg_stack(screen, WIDTH / 2, 75, self.text_blocks, 14, "green")
+
+            draw_text(screen, self.opponent_text, 14, WIDTH / 2, self.current_opponent_text_height, 'red')
+            draw_msg_stack(screen, WIDTH / 2, 245, self.opponent_text_blocks, 14, "red")
+
+            # after drawing everything, flip the display
+            pygame.display.flip()
+
+    # Main loop
+    def run(self):
+
+        while not self.game_over:
+            self.event_handler()
+            self.draw()
+
+        pygame.quit()
